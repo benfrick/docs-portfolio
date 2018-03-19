277597f99f0d207d4d01c137f9c48bdd2902059a
Tim Kye <Timothy.Kye@nike.com>
2018-03-19 13:48:30 -0700

    add cloudfront for ssl, add deploy script

--- pipeline/terraform/s3-site.tf
+++ pipeline/terraform/s3-site.tf
@@ -1,7 +1,8 @@
 resource "aws_s3_bucket" "site" {
-    bucket = "${local.domain_name}"
-    acl = "public-read"
-    policy = <<EOF
+  bucket = "${local.domain_name}"
+  acl    = "public-read"
+
+  policy = <<EOF
 {
   "Id": "bucket_policy_site",
   "Version": "2012-10-17",
@@ -18,26 +19,110 @@ resource "aws_s3_bucket" "site" {
   ]
 }
 EOF
-    website {
-        index_document = "index.html"
-        error_document = "404.html"
-    }
-    tags {
-    }
-    force_destroy = true
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
+
+  tags {}
+
+  force_destroy = true
 }
 
+# don't create the domain yet, DNS migration is pending
 resource "aws_route53_record" "domain" {
-   name = "${local.domain_name}"
-   zone_id = "${local.hosted_zone_id}"
-   type = "A"
-   alias {
-      name = "s3-website-${var.region}.amazonaws.com."
-      zone_id = "${aws_s3_bucket.site.hosted_zone_id}"
-    #  name = "${var.route53_domain_alias_name}"
-    #  zone_id = "${var.hosted_zone_id}"
-    #  name = "${aws_cloudfront_distribution.website_cdn.domain_name}"
-    #  zone_id = "${aws_cloudfront_distribution.website_cdn.hosted_zone_id}"
-      evaluate_target_health = true
-   }
+  name    = "${local.domain_name}"
+  zone_id = "${local.hosted_zone_id}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_cloudfront_distribution.website_cdn.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.website_cdn.hosted_zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_cloudfront_distribution" "website_cdn" {
+  enabled         = true
+  is_ipv6_enabled = true
+  price_class     = "PriceClass_100"
+
+  # http_version = "http2"
+
+  "origin" {
+    origin_id = "origin-bucket-${aws_s3_bucket.site.id}"
+
+    # domain_name = "${aws_s3_bucket.site.website_endpoint}"
+    domain_name = "${local.domain_name}.s3.amazonaws.com"
+
+    custom_origin_config {
+      origin_protocol_policy = "http-only"
+      http_port              = "80"
+      https_port             = "443"
+      origin_ssl_protocols   = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+  default_root_object = "index.html"
+  custom_error_response {
+    error_code = "404"
+
+    # error_caching_min_ttl = "360"
+    response_code      = "200"
+    response_page_path = "/index.html"
+  }
+  "default_cache_behavior" {
+    allowed_methods = ["GET", "HEAD", "DELETE", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods  = ["GET", "HEAD"]
+
+    "forwarded_values" {
+      query_string = "true"
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl          = "0"
+    default_ttl      = "300"                                    //3600
+    max_ttl          = "1200"                                   //86400
+    target_origin_id = "origin-bucket-${aws_s3_bucket.site.id}"
+
+    // This redirects any HTTP request to HTTPS. Security first!
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+  }
+  cache_behavior {
+    allowed_methods = ["GET", "HEAD", "DELETE", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = "true"
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl          = "0"
+    default_ttl      = "0"
+    max_ttl          = "0"
+    target_origin_id = "origin-bucket-${aws_s3_bucket.site.id}"
+    path_pattern     = "/index.html"
+
+    // This redirects any HTTP request to HTTPS. Security first!
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+  }
+  "restrictions" {
+    "geo_restriction" {
+      restriction_type = "none"
+    }
+  }
+  "viewer_certificate" {
+    acm_certificate_arn      = "${local.domain_certificate_arn}"
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1"
+  }
+  aliases = ["${local.domain_name}"]
 }
--- pipeline/terraform/variables.tf
+++ pipeline/terraform/variables.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-      key    = "nde-devportal-docs"
-      region = "us-west-2"
-      workspace_key_prefix = "nde-devportal-docs"
-    }
+  backend "s3" {
+    key                  = "nde-devportal-docs"
+    region               = "us-west-2"
+    workspace_key_prefix = "nde-devportal-docs"
+  }
 }
 
 variable "region" {
@@ -15,6 +15,7 @@ provider "aws" {
 }
 
 locals {
-  domain_name = "nde-devportal-docs.niketech.com"
-  hosted_zone_id = "Z7OWM2GFULVOF"
+  domain_name            = "nde-devportal-docs.niketech.com"
+  hosted_zone_id         = "Z7OWM2GFULVOF"
+  domain_certificate_arn = "arn:aws:acm:us-east-1:370680818641:certificate/03bf66ad-dd3b-414a-a20e-45b2403ba6a2"
 }
