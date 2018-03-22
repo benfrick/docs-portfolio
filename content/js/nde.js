eb63fd66565cb4c8ea49dd5525871f35417e2e35
jane moore <jane.moore@nike.com>
2018-03-22 13:55:28 -0700

    APID-280: added ready to listen message

--- content/js/nde.js
+++ content/js/nde.js
@@ -1,10 +1,12 @@
 var allowed = ['https://developer.preprod.niketech.com','https://developer.niketech.com'];
+var parent = window.opener;
 
 function receiveMessage(event)
 {
   console.log('received event from origin ' + event.origin);
   console.log('received data ' + event.data);
   for (i = 0; i < allowed.length; i++) {
+    console.log('comparing event.origin '+ event.origin + ' to allowed ' + allowed[i]);
       if (event.origin === allowed[i]){
         processMessage (event);
         break;
@@ -12,24 +14,20 @@ function receiveMessage(event)
   }
   console.log('done receiving message');
 }
-
 function processMessage (event) {
   console.log('allowing origin ' + event.origin);
 
-  //send receipt message to sender
-  sendMessage('received message', event.origin, event.source);
-
-  //load requested url
-  //e.g./doc/commerce/checkout/api_checkout.html
   location.assign(event.data);
 
-  console.log('done processing message');
+  console.log('done processing message ' + event.data);
 
 }
-
-function sendMessage (message,origin,recipient){
+function sendMessage (message, origin, recipient){
     recipient.postMessage(message, origin, false);
-    console.log('done sending message to ' + origin);
+    console.log('done sending message ' + message + ' to ' + origin);
 }
 window.addEventListener('message', receiveMessage, false);
 console.log('added event listener');
+if(parent)
+    parent.sendMessage(location.href,location.origin);
+console.log('notified parent ready to listen');
